32c55bad57a7c9411e58bd8bfdc293286e2911fb
LuisPedroChaves <luispedrochaves@hotmail.com>
2019-04-30 19:14:09 -0600

    New User

---
+++ newUser.php
@@ -0,0 +1,13 @@
+<?php
+include_once 'functions/sesiones.php';
+include_once 'templates/header.php';
+include_once 'templates/navbar.php';
+include_once 'templates/sidebar.php';
+include_once 'functions/bd_conexion.php';
+?>
+
+puta
+
+<?php
+include_once 'templates/footer.php';
+?>
--- templates/footer.php
+++ templates/footer.php
@@ -1,19 +1,41 @@
 </div><!-- /.app -->
-    <!-- BEGIN BASE JS -->
-    <script src="assets/vendor/jquery/jquery.min.js"></script>
-    <script src="assets/vendor/bootstrap/js/popper.min.js"></script>
-    <script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script> <!-- END BASE JS -->
-    <!-- BEGIN PLUGINS JS -->
-    <script src="assets/vendor/pace/pace.min.js"></script>
-    <script src="assets/vendor/stacked-menu/stacked-menu.min.js"></script>
-    <script src="assets/vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
-    <script src="assets/vendor/flatpickr/flatpickr.min.js"></script>
-    <script src="assets/vendor/easy-pie-chart/jquery.easypiechart.min.js"></script>
-    <script src="assets/vendor/chart.js/Chart.min.js"></script> <!-- END PLUGINS JS -->
-    <!-- BEGIN THEME JS -->
-    <script src="assets/javascript/theme.min.js"></script> <!-- END THEME JS -->
-    <!-- BEGIN PAGE LEVEL JS -->
-    <script src="assets/javascript/pages/dashboard-demo.js"></script> <!-- END PAGE LEVEL JS -->
-  </body>
+<!-- .app-footer -->
+<footer class="app-footer">
+    <!-- <ul class="list-inline">
+        <li class="list-inline-item">
+            <a class="text-muted" href="#">Support</a>
+        </li>
+        <li class="list-inline-item">
+            <a class="text-muted" href="#">Help Center</a>
+        </li>
+        <li class="list-inline-item">
+            <a class="text-muted" href="#">Privacy</a>
+        </li>
+        <li class="list-inline-item">
+            <a class="text-muted" href="#">Terms of Service</a>
+        </li>
+    </ul> -->
+    <div class="copyright"> <strong>Copyright &copy; 2019 - <?php echo date("Y"); ?> APPLITECH SOFTWARE SOLUTIONS.
+        </strong>
+        Todos Los
+        Derechos Reservados. </div>
+</footer><!-- /.app-footer -->
+<!-- BEGIN BASE JS -->
+<script src="assets/vendor/jquery/jquery.min.js"></script>
+<script src="assets/vendor/bootstrap/js/popper.min.js"></script>
+<script src="assets/vendor/bootstrap/js/bootstrap.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@8"></script> <!-- END BASE JS -->
+<!-- BEGIN PLUGINS JS -->
+<script src="assets/vendor/pace/pace.min.js"></script>
+<script src="assets/vendor/stacked-menu/stacked-menu.min.js"></script>
+<script src="assets/vendor/perfect-scrollbar/perfect-scrollbar.min.js"></script>
+<script src="assets/vendor/flatpickr/flatpickr.min.js"></script>
+<script src="assets/vendor/easy-pie-chart/jquery.easypiechart.min.js"></script>
+<script src="assets/vendor/chart.js/Chart.min.js"></script> <!-- END PLUGINS JS -->
+<!-- BEGIN THEME JS -->
+<script src="assets/javascript/theme.min.js"></script> <!-- END THEME JS -->
+<!-- BEGIN PAGE LEVEL JS -->
+<script src="assets/javascript/pages/dashboard-demo.js"></script> <!-- END PAGE LEVEL JS -->
+</body>
+
 </html>
